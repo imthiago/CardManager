237b7bbe2b8c7c13e1fe98a0369e983a63987ea1
Thiago Oliveira <thiago@outlook.com.br>
2021-03-02 21:24:14 -0300

    ViewCode configs and adding swiftlint exclusions

--- CardManager/LaunchScreenViewController.swift
+++ CardManager/LaunchScreenViewController.swift
@@ -7,11 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LaunchScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 }
