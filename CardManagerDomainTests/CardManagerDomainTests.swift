8bcd96fb1e83569094c2c35ea8ba78c6dca293ec
Thiago Oliveira <thiago@outlook.com.br>
2021-03-10 22:41:30 -0300

    Adding default test class and info.plist to target

---
+++ CardManagerDomainTests/CardManagerDomainTests.swift
@@ -0,0 +1,15 @@
+//
+//  CardManagerDomainTests.swift
+//  CardManagerDomainTests
+//
+//  Created by Thiago Oliveira on 10/03/21.
+//
+
+import XCTest
+
+class CardManagerDomainTests: XCTestCase {
+
+    func test() {
+
+    }
+}
