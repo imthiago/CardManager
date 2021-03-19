e6a1790b71b6ea84b8c5f1bffe6899513fe92323
Thiago Oliveira <thiago@outlook.com.br>
2021-03-18 22:57:33 -0300

    Adding property to use inside viewModels

---
+++ CardManagerPresentation/Property.swift
@@ -0,0 +1,28 @@
+//
+//  Property.swift
+//  CardManagerPresentation
+//
+//  Created by Thiago Oliveira on 18/03/21.
+//
+
+class Property<Type> {
+    private var subscribeAction: (Type?) -> Void = { _ in }
+
+    func subscribe(_ closure: @escaping (Type?) -> Void) {
+        subscribeAction = closure
+
+        if let value = value {
+            self.subscribeAction(value)
+        }
+    }
+
+    var value: Type? {
+        didSet {
+            subscribeAction(value)
+        }
+    }
+
+    init(_ val: Type) {
+        value = val
+    }
+}
